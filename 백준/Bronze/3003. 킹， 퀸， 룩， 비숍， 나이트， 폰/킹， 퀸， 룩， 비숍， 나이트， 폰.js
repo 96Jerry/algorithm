83d668674d933a910837f746d5c20857794615bd
June Lim <98335005+96Jerry@users.noreply.github.com>
2023-09-05 23:45:10 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
-input = fs.readFileSync(filePath).toString().split(" ");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().split(" ");
 
-criteria = [1, 1, 2, 2, 2, 8];
+const criteria = [1, 1, 2, 2, 2, 8];
 
-answer = [];
+const answer = [];
 for (let i = 0; i < 6; i++) {
   answer.push(criteria[i] - input[i]);
 }

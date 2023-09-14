b1641e0b72c7616e3929419552adf3ebcfbbf970
June Lim <98335005+96Jerry@users.noreply.github.com>
2023-09-14 21:03:42 +0900

    [Silver V] Title: 너의 평점은, Time: 144 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const criteria = new Map([
+  ["A+", 4.5],
+  ["A0", 4.0],
+  ["B+", 3.5],
+  ["B0", 3.0],
+  ["C+", 2.5],
+  ["C0", 2.0],
+  ["D+", 1.5],
+  ["D0", 1.0],
+  ["F", 0],
+]);
+
+let total = 0;
+let sum = 0;
+let count = 0;
+for (const subject of input) {
+  const subjectList = subject.split(" ");
+  const subjectNumber = Number(subjectList[1]);
+  const subjectWord = subjectList[2];
+
+  if (subjectWord === "P") {
+    count += 1;
+    continue;
+  }
+
+  total += subjectNumber;
+  sum += Number(criteria.get(subjectWord)) * subjectNumber;
+}
+
+const answer = sum / total;
+console.log(answer);

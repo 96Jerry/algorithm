cc4d036d990c9d25eeb007308afdd3cd2ac82020
June Lim <98335005+96Jerry@users.noreply.github.com>
2025-09-06 22:16:27 +0900

    Time: 6 ms (18.02%), Space: 58.1 MB (8.89%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = []
+    const closeBrackets = [')', '}', ']']
+    const mapBracket = new Map([['(', ')'], ['{', '}'], ['[', ']']])
+    let flag = true
+    // let closeBracketCount = 0
+
+
+    if (s.length === 1) {
+        return false
+    }
+
+    for (const str of [...s]) {
+        flag = true
+        if (closeBrackets.includes(str)) {
+            // closeBracketCount++
+            if (mapBracket.get(stack[stack.length - 1]) !== str) {
+                return false
+            }
+
+            stack.splice(stack.length - 1, 1)
+            flag = false
+        }
+
+        if(flag === true){
+            stack.push(str)
+        }
+        
+    }
+
+    if(stack.length > 0){
+        return false
+    }
+
+    return true
+};

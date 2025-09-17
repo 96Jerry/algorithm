3fb926bd2a10265a55c81e1fec1521f4b52c1426
June Lim <98335005+96Jerry@users.noreply.github.com>
2025-09-17 20:23:38 +0900

    Time: 1 ms (97.97%), Space: 57.8 MB (59.37%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi, '')
+    if (s.length === 0) {
+        return true
+    }
+
+    if ([...s].reverse().join('') === s) {
+        return true
+    }
+
+    return false
+};

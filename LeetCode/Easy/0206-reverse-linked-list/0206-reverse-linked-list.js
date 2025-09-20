6ed5baa8fe283287f02a28e970627eeb82dfb0be
June Lim <98335005+96Jerry@users.noreply.github.com>
2025-09-20 18:16:27 +0900

    Time: 0 ms (100%), Space: 56.7 MB (72.01%) - LeetHub

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.js
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,25 +10,14 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    if (!head) {
-        return head
+    let prev = null
+    let curr = head
+    while (curr) {
+        let next = curr.next
+        curr.next = prev // 역방향 전환
+        prev = curr
+        curr = next
     }
 
-    const init = new ListNode()
-    init.val = head.val
-    init.next = null
-
-    let next = head.next
-    let prev = init
-    let cur = init
-    while (next) {
-        cur = new ListNode()
-        cur.val = next.val
-        cur.next = prev
- 
-        prev = cur
-        next = next.next
-    }
-
-    return cur
+    return prev
 };

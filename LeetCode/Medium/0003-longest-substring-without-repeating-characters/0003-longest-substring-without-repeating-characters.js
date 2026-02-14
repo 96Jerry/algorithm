3c88c4334f3bcb7ad9a6441688972722db6feab3
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-14 12:42:24 +0900

    Time: 5 ms (78.32%), Space: 57 MB (82.43%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,24 +3,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    // 별도 변수를 만들어서 s를 앞에서부터 순서대로 한글자씩 넣어준다.
-    // 중복되는 문자가 나오지 않으면 계속 answer를 업데이트 해준다.
-    // 중복되는 문자가 나오면 별도 변수의 맨앞을 제거하고 s에 대해 다시 작업을 반복한다.
-    const sArray = s.split("");
+    let answer = 0
+    let left = 0
+    let right = 0
+    const uniqueWords = new Set()
 
-    let uniqueArray = [];
-    let answer = 0;
-    while (sArray.length > 0) {
-        uniqueArray.push(sArray.shift());
-
-        const isUniqueArrayUnique =
-            uniqueArray.length === new Set(uniqueArray).size;
-        if (isUniqueArrayUnique) {
-            answer = Math.max(uniqueArray.length, answer);
-        } else {
-            uniqueArray.shift();
+    while (left <= right && right < s.length) {
+        while (uniqueWords.has(s[right])) {
+            uniqueWords.delete(s[left])
+            left++
         }
+        uniqueWords.add(s[right])
+        answer = Math.max(right - left + 1, answer)
+        right++
     }
 
-    return answer;
+    return answer
 };

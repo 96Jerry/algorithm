96f041d5265cb0be92215d87fa4a1db9c33eca65
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-16 09:48:09 +0900

    Time: 19 ms (57.7%), Space: 54.3 MB (98.61%) - LeetHub

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -3,25 +3,25 @@
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function(s, k) {
-    let left = 0;
-    let maxCount = 0;
-    let maxLength = 0;
-    const charCount = {};
-    
+var characterReplacement = function (s, k) {
+    let left = 0
+    let maxCount = 0
+    let maxLength = 1
+    const charCount = {}
+
     for (let right = 0; right < s.length; right++) {
-        const char = s[right];
-        charCount[char] = (charCount[char] || 0) + 1;
-        
-        maxCount = Math.max(maxCount, charCount[char]);
-        
+        const char = s[right]
+        charCount[char] = (charCount[char] || 0) + 1
+        maxCount = Math.max(charCount[char], maxCount)
+
         while (right - left + 1 - maxCount > k) {
-            charCount[s[left]]--;
-            left++;
+            charCount[s[left]] -= 1
+            left++
+            continue
         }
-        
-        maxLength = Math.max(maxLength, right - left + 1);
+
+        maxLength = Math.max(right - left + 1, maxLength)
     }
-    
-    return maxLength;
+
+    return maxLength
 };

00eb85097cd7fb7cfea41aa5a97160891cc32f36
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-21 13:01:39 +0900

    Time: 2 ms (69.35%), Space: 55.7 MB (28.23%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.js
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.js
@@ -4,9 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    // nums를 순회하면서 하나 고르면 다른 하나 정해짐. 있으면 출력
+    const numIndexMap = new Map()
+
     for (let i = 0; i < nums.length; i++) {
-        const j = nums.indexOf(target - nums[i])
-        if (j > -1 && j !== i) return [i, j]
+        if (numIndexMap.has(target - nums[i])) return [i, numIndexMap.get(target - nums[i])]
+
+        numIndexMap.set(nums[i], i)
     }
 };

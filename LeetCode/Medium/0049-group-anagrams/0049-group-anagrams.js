0ecb380f57bb01b886da359dbed8f630ead6108e
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-21 14:13:35 +0900

    Time: 34 ms (52.38%), Space: 66.7 MB (84.85%) - LeetHub

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    const map = new Map()
+
+    for (const str of strs) {
+        const strSorted = [...str].toSorted().join()
+        if (!map.has(strSorted)) {
+            map.set(strSorted, [str])
+        } else {
+            map.get(strSorted).push(str)
+        }
+    }
+
+    return [...map].map(([key, value]) => value)
+};

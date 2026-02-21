b0da04f8ba8172b1ac2cb3f3141531b09c4b46d0
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-21 16:04:45 +0900

    Time: 36 ms (58.5%), Space: 73.3 MB (25.29%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.js
+++ LeetCode/Medium/0015-3sum/0015-3sum.js
@@ -3,34 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    const results = []
+    // 하나를 정해놓고 나머지 두개를 투포인터로 찾기
+    // 정렬해서 중복 제거 및 투포인터 활용
+    const result = []
+
     nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) continue
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        // 중복 건너뛰기
-        if (i > 0 && nums[i] === nums[i - 1]) continue
-
-        left = i + 1
-        right = nums.length - 1
+        let left = i + 1
+        let right = nums.length - 1
         while (left < right) {
-            sum = nums[i] + nums[left] + nums[right]
-
+            const sum = nums[i] + nums[left] + nums[right]
             if (sum === 0) {
-                results.push([nums[i], nums[left], nums[right]])
-
-                // 중복 건너뛰기
-                while (nums[left + 1] === nums[left]) left++
-                while (nums[right - 1] === nums[right]) right--
-
+                result.push([nums[i], nums[left], nums[right]])
                 left++
                 right--
+                while (nums[left] === nums[left-1]) left++
+                while (nums[right] === nums[right+1]) right--
             } else if (sum < 0) {
                 left++
-            } else {
+            } else if (sum > 0) {
                 right--
             }
         }
     }
 
-    return results
+    return result
 };

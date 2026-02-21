92f65d03259ca7bd8547d5b149605dc9a0dee0fa
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-21 16:59:38 +0900

    Time: 4 ms (15.25%), Space: 63.5 MB (35.53%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.js
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,25 +3,19 @@
  * @return {number}
  */
 var maxArea = function (height) {
-    var calSize = function (i, j) {
-        if (i === j) return -1
-
-        return Math.abs(i - j) * Math.min(height[i], height[j])
-    }
-
     let left = 0
     let right = height.length - 1
-    let maxWaterSize = -1
 
+    let answer = Math.min(height[left], height[right]) * (right - left)
     while (left < right) {
-        maxWaterSize = Math.max(maxWaterSize, calSize(left, right))
-
-        if (height[left] < height[right]) {
+        if (height[left] <= height[right]) {
             left++
-        } else {
+            answer = Math.max(answer, Math.min(height[left], height[right]) * (right - left))
+        } else if (height[left] > height[right]) {
             right--
+            answer = Math.max(answer, Math.min(height[left], height[right]) * (right - left))
         }
     }
 
-    return maxWaterSize
+    return answer
 };

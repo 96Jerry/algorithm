39c0e6cc2c48d4d94801292554d31b2a4e535498
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-02-23 13:20:07 +0900

    Time: 7 ms (55.58%), Space: 68.6 MB (87.34%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,17 +3,35 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    if (nums.filter(el => el === 0).length === 1) {
-        const array = new Array(nums.length).fill(0)
-        array[nums.indexOf(0)] = nums.reduce((acc, cur) => cur === 0 ? acc : acc * cur, 1)
-
-        return array
-    }
-
-    const product = nums.reduce((acc, cur) => acc * cur, 1)
+    let product = 1
+    let zeroCount = 0
+    const zeroIndex = []
     for (let i = 0; i < nums.length; i++) {
-        nums[i] = nums[i] === 0 ? 0 : product / nums[i]
+        if (nums[i] === 0) {
+            zeroCount++
+            zeroIndex.push(i)
+            if (zeroCount >= 2) {
+                break
+            }
+            continue
+        }
+
+        product *= nums[i]
     }
 
-    return nums
+    const answer = new Array(nums.length)
+    for (let i = 0; i < nums.length; i++) {
+        if (zeroCount >= 2) {
+            answer.fill(0)
+            break
+        } else if (zeroCount === 1) {
+            answer.fill(0)
+            answer[zeroIndex[0]] = product
+            break
+        } else {
+            answer[i] = product / nums[i]
+        }
+    }
+
+    return answer
 };

c5be54eb35297e1cfcb1c4c751f2a97bf938f60e
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-06-29 20:39:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.js
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.js
@@ -4,14 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numsMap = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        numsMap.set(nums[i], i)
-    }
+    nums.sort((a, b) => a - b)
 
-    for (let i = 0; i < nums.length; i++) {
-        if (numsMap.has(target - nums[i]) && i !== numsMap.get(target - nums[i])) {
-            return [i, numsMap.get(target - nums[i])]
+    let left = 0
+    let right = nums.length - 1
+    while (left < right) {
+        const num1 = nums[left]
+        const num2 = nums[right]
+        const sum = num1 + num2
+        if (sum > target) {
+            right--
+        } else if (sum < target) {
+            left++
+        } else {
+            return [left, right]
         }
     }
 };

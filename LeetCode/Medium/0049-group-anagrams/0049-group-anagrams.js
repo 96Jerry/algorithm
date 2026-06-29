1ae868406a8bef0bcf01c94209b8c5e92c8576c5
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-06-29 20:39:27 +0900

    Time: 39 ms (30.87%), Space: 69.4 MB (16.13%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -2,17 +2,29 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    const map = new Map()
+var groupAnagrams = function(strs) {
+    // strs 돌면서 str 정렬
+    // 새로운 배열(정렬된 strs)에 대해 해시맵(애나그램 집합)에 넣음. key=애나그램, value=인덱스
+    // strs를 돌면서 출력물을 뽑아냄
 
-    for (const str of strs) {
-        const strSorted = [...str].toSorted().join('')
-        if (!map.has(strSorted)) {
-            map.set(strSorted, [str])
+    const sortedStrs = strs.map((str) => [...str].sort().join(''))
+    const anagramMap = new Map()
+    for (let i = 0; i < sortedStrs.length; i++) {
+        if (!anagramMap.has(sortedStrs[i])) {
+            anagramMap.set(sortedStrs[i], [i])
         } else {
-            map.get(strSorted).push(str)
+            anagramMap.get(sortedStrs[i]).push(i)
         }
     }
 
-    return [...map.values()]
+    const answer = []
+    for (const [key, value] of anagramMap) {
+        const subAnswer = []
+        for (const index of value) {
+            subAnswer.push(strs[index])
+        }
+        answer.push(subAnswer)
+    }
+    
+    return answer
 };

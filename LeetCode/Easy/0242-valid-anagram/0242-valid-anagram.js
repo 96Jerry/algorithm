b21f0a0e822ace47a0db9ec5a7d6d15462f1a977
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-07-03 16:23:42 +0900

    Time: 8 ms (89.27%), Space: 54.8 MB (89.99%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
@@ -7,13 +7,11 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
 
     const base = 'a'.charCodeAt(0)
-
-    const anagramCodeMaker1 = new Array(26).fill(0)
-    const anagramCodeMaker2 = new Array(26).fill(0)
+    const count = new Array(26).fill(0)
     for (let i = 0; i < s.length; i++) {
-        anagramCodeMaker1[s.charCodeAt(i) - base] += 1
-        anagramCodeMaker2[t.charCodeAt(i) - base] += 1
+        count[s.charCodeAt(i) - base] += 1
+        count[t.charCodeAt(i) - base] -= 1
     }
 
-    return anagramCodeMaker1.join('') === anagramCodeMaker2.join('')
+    return count.every((el) => el === 0)
 };

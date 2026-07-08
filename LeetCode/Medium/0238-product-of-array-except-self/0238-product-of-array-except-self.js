9a739856a24f9d44cd11e7059c2329b59d2a0412
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-07-09 08:50:23 +0900

    Time: 5 ms (78.03%), Space: 70.7 MB (56.63%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,36 +2,35 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function (nums) {
+var productExceptSelf = function(nums) {
+    // 0이 없으면 다 곱한 값에서 각 원소를 나눔
+    // 0이 하나만 있으면 원래대로 하되 0이 아닌 부분은 다 0
+    // 0이 두개 이상 있으면 다 0
+
+    // 0을 세서 두개 이상 있으면 다 0인 배열 반환
+    // 하나만 있으면 0 빼고 다 곱한 값을 그곳에 넣고 나머지는 다 0 넣어서 배열 반환
+    // 0이 없으면 다 곱한 값을 구하고 각 원소로 나눈 배열 반환
+    // 0을 세면서 동시에 answer를 채울 수 있을까?
+    // 나누려는 원소가 0이면 나누지 않고
+    // 어쨌든 모두 곱한 값, 각 원소로 나눈 값이 필요해서 두번은 돌아야 함
+    const zeroIndexes = []
     let product = 1
-    let zeroCount = 0
-    const zeroIndex = []
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 0) {
-            zeroCount++
-            zeroIndex.push(i)
-            if (zeroCount >= 2) {
-                break
-            }
-            continue
-        }
-
-        product *= nums[i]
-    }
-
-    const answer = new Array(nums.length)
-    for (let i = 0; i < nums.length; i++) {
-        if (zeroCount >= 2) {
-            answer.fill(0)
-            break
-        } else if (zeroCount === 1) {
-            answer.fill(0)
-            answer[zeroIndex[0]] = product
-            break
+            zeroIndexes.push(i)
         } else {
-            answer[i] = product / nums[i]
+            product *= nums[i]
         }
     }
 
-    return answer
+    if (zeroIndexes.length >= 2) {
+        return new Array(nums.length).fill(0)
+    } else if (zeroIndexes.length === 1) {
+        const answer = new Array(nums.length).fill(0)
+        answer[zeroIndexes[0]] = product
+        return answer
+    } else {
+        const answer = nums.map((num) => product / num)
+        return answer
+    }
 };

9bcd61ce7d0d7d5fa2f0a6ccf7831b022b71610e
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-07-09 09:11:27 +0900

    Time: 4 ms (86.42%), Space: 72.2 MB (27.09%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,34 +3,24 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    // 0이 없으면 다 곱한 값에서 각 원소를 나눔
-    // 0이 하나만 있으면 원래대로 하되 0이 아닌 부분은 다 0
-    // 0이 두개 이상 있으면 다 0
+    const n = nums.length;
+    const answer = new Array(n);
 
-    // 0을 세서 두개 이상 있으면 다 0인 배열 반환
-    // 하나만 있으면 0 빼고 다 곱한 값을 그곳에 넣고 나머지는 다 0 넣어서 배열 반환
-    // 0이 없으면 다 곱한 값을 구하고 각 원소로 나눈 배열 반환
-    // 0을 세면서 동시에 answer를 채울 수 있을까?
-    // 나누려는 원소가 0이면 나누지 않고
-    // 어쨌든 모두 곱한 값, 각 원소로 나눈 값이 필요해서 두번은 돌아야 함
-    const zeroIndexes = []
-    let product = 1
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            zeroIndexes.push(i)
-        } else {
-            product *= nums[i]
-        }
+    // 1패스(왼→오): answer[i] = i 왼쪽 원소들의 곱
+    // 대입을 먼저 하고 곱하므로, nums[i] 자신은 절대 포함되지 않는다
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        answer[i] = prefix;
+        prefix *= nums[i];
     }
 
-    if (zeroIndexes.length >= 2) {
-        return new Array(nums.length).fill(0)
-    } else if (zeroIndexes.length === 1) {
-        const answer = new Array(nums.length).fill(0)
-        answer[zeroIndexes[0]] = product
-        return answer
-    } else {
-        const answer = nums.map((num) => product / num)
-        return answer
+    // 2패스(오→왼): 오른쪽 곱을 변수 하나로 누적하며 answer에 곱해준다
+    // 별도 right 배열이 필요 없다 → extra space O(1)
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
     }
+
+    return answer;
 };

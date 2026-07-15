dca7949c3c6a231b6e78c2c7dd01420b450d018d
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-07-15 21:01:01 +0900

    Time: 10 ms (13.28%), Space: 60.2 MB (8.74%) - LeetHub

--- LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.js
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,59 +2,60 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
-    const isRepeatable = (doubleArray) => {
-        for (const array of doubleArray) {
-            if (array.length !== 9) {
-                throw new Error('wrong parameter')
-            }
+var isValidSudoku = function(board) {
+    // 현재 board에 채워진 숫자들을 기반으로 유효한 스도쿠판인지 판단
+    // 유효한 지와 풀 수 있는 지는 다름
+    // 유효하다 = 있는 숫자가 명시된 조건 3개를 만족한다
+    // 모든 행, 열, 33칸에 대해 조건을 확인한다 27번
+    // 같은 부분이 한 곳이라도 있으면 무효
+    // 없는 부분인 "."를 제외하고 set에 넣어서 길이가 다르면 같은 부분이 있다는 뜻
 
-            const numbers = array.filter((el) => el !== '.')
-            if (numbers.length === 9) {
-                return false
-            }
-
-            for (const number of numbers) {
-                if (numbers.filter(el => el === number).length > 1) {
-                    return false
-                }
-            }
+    function isSameExists(array) {
+        let compare = new Set()
+        for (const a of array) {
+            if (a === ".") continue
+            if (compare.has(a)) return true
+            compare.add(a)
         }
 
-        return true
+        return false
     }
 
-    const rows = []
-    const columns = [[".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."]]
-    const boxes = [[".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."]]
+    // 행 확인
+    for (let i = 0; i < 9; i++) {
+        const isColValid = !isSameExists(board[i])
+        if (isColValid === false) return false
+    }
 
+    // 열 확인
+    const rows = []
     for (let i = 0; i < 9; i++) {
         const row = []
         for (let j = 0; j < 9; j++) {
-            row.push(board[i][j])
-            columns[j][i] = board[i][j]
-            boxes[i][j] = board[Math.floor(i / 3) * 3 + Math.floor(j / 3)][(i % 3) * 3 + j % 3]
-            // (i, j)(4, 3) -> (5, 4) // (1, 1) -> (Math.floor(i/3), Math.floor(j/3))
-            // (i, j)(1, 1) -> (0, 4)
+            row.push(board[j][i])
         }
         rows.push(row)
     }
+    for (let i = 0; i < 9; i++) {
+        const isRowValid = !isSameExists(rows[i])
+        if (isRowValid === false) return false
+    }
 
-    return isRepeatable(rows) && isRepeatable(columns) && isRepeatable(boxes)
+    // 33칸 확인
+    // 3으로 나눴을 때 몫이 같은 인덱스의 것끼리 묶기?
+    const squares = Array.from({ length: 9 }, () => [])
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // i와 j로 square에서의 위치 특정하기 0 ~ 9
+            // 0,0 0,1 1,0 1,1 2,0 0,2 2,1 1,2 2,2
+            // x값에 3을 부여
+            squares[3 * Math.floor(i/3) + Math.floor(j/3)].push(board[i][j])
+        }
+    }
+    for (let i = 0; i < 9; i++) {
+        const isSquareValid = !isSameExists(squares[i])
+        if (isSquareValid === false) return false
+    }
+
+    return true
 };

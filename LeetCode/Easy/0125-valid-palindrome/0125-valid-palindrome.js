6745a052a190047b9b03b0982d9ce133fe17e8b1
June Lim <98335005+96Jerry@users.noreply.github.com>
2026-07-18 15:22:48 +0900

    Time: 10 ms (16.94%), Space: 61.7 MB (16.61%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,7 +4,6 @@
  */
 var isPalindrome = function (s) {
     const a = [...s].filter((el) => (el.charCodeAt(0) >= 'A'.charCodeAt(0) && el.charCodeAt(0) <= 'Z'.charCodeAt(0)) || (el.charCodeAt(0) >= 'a'.charCodeAt(0) && el.charCodeAt(0) <= 'z'.charCodeAt(0)) || (el.charCodeAt(0) >= '0'.charCodeAt(0) && el.charCodeAt(0) <= '9'.charCodeAt(0))).map((el) => el.toLowerCase())
-    console.log(a)
     for (let i = 0; i < Math.floor(a.length / 2); i++) {
         if (a[i] !== a[a.length - 1 - i]) return false
     }
